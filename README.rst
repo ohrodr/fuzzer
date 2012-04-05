062d6bdaf8cf5fa1f3dc3f91ad7e456620603d27
rodr <rodr@obvsly.me>
2012-04-05 02:35:03 -0700

    rodr

--- README.rst
+++ README.rst
@@ -1,7 +1,8 @@
-Author  : Robb Driscoll
-License : GPLv3
-Email   : ohrodr@gmail.com
-Date    : 4/5/12
+Robb Driscoll
+License - GPLv3
+
+Email   - ohrodr@gmail.com
+Date    - 4/5/12
 
 I'm putting together some random code this evening.  Threw together the basics of a number fuzzer.
 
--- numfuz.py
+++ numfuz.py
@@ -1,24 +1,48 @@
+###
+#   Author  : Robb Driscoll
+#   License : GPLv3
+#   Email   : ohrodr@gmail.com
+#   Date    : 4/5/12
+###
+
 class NumFuzz():
     """
     This is a very basic number fuzzer.
-    the idea is to use permutations of factors of 2.
+    The idea is to use permutations of factors of 2.
     The permutations, or extended methods are for added alteration of the possible digits returned.
     This data can be fed to something else.
     returns of all methods are generators
+
+    USAGE:
+        >>> import numfuz
+        >>> nf = numfuz.NumFuzz()
+        >>> nf.fuznumber()
+        <generator object fuznumber at 0xb728f02c>
+        >>> for i in nf.fuznumber(): print i
+        ...
+        >>> for i in nf.doubles(): print i
+        ...
+        >>> for i in nf.doubles_extended(): print i
+        ...
+
     """
 
     def fuznumber(self):
         """
-        this method returns a fuzzing that contains all the numbers from the simple sets
+        This method returns a fuzzing that contains all the numbers from associated methods
+        return is generator
         """
         result = []
+        # append self.doubles() output
         for i in self.doubles(): result.append(i)
+        # append self.doubles_extended() output
         for i in self.doubles_extended(): result.append(i)
         for i in result: yield i
 
     def doubles(self):
         """
         This returns simply powers of two
+        return is generator
         """
         result = [
                 0x00000001,
@@ -40,9 +64,10 @@ class NumFuzz():
     def doubles_extended(self):
         """
         This method returns powers of two permutated
+        return is generator
         """
         result = []
-        for i in self._doubles():
+        for i in self.doubles():
             result.append(i + 1)
             result.append(i + 2)
             result.append(i - 1)
